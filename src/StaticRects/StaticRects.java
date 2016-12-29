e52017e2a3be2ebf70535cb4d77718e7d37ef244
kmne68 <kmne68@gmail.com>
2016-12-28 22:08:53 -0600

    Add StaticRects class to draw rectangles

--- src/StaticRects/StaticRects.java
+++ src/StaticRects/StaticRects.java
@@ -4,11 +4,40 @@
  */
 package StaticRects;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /**
  *
  * @author Keith
  */
 public class StaticRects extends AnimationBase {
     
-    
-}
+    public void drawFrame(Graphics g) {
+        
+        // Draw nested black rectangles on a red background.
+        // Separated rectangles by 15 px
+        // Frame is 300 x 160.
+        
+        int inset;                      // gap between border and rectangles
+        int rectWidth, rectHeight;      // rectangle dimensions
+        
+        g.setColor(Color.red);
+        g.fillRect(0, 0, 300, 160);
+        
+        g.setColor(Color.BLACK);
+        
+        inset = 0;
+        
+        rectWidth = 299;
+        rectHeight = 159;
+        
+        while(rectWidth >= 0 && rectHeight >= 0) {
+            
+            g.drawRect(inset, inset, rectWidth, rectHeight);
+            inset += 15;
+            rectWidth -= 30;
+            rectHeight -= 30;
+        }
+    } // end paint()
+} // end class StaticRects
