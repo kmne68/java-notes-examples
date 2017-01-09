ed9e8775c9ca16d56c6f5f01fdb30f141f4545db
kmne68 <kmne68@gmail.com>
2017-01-08 23:42:53 -0600

    Add tile splitter, basic test on map

--- src/StaticRects/Map.java
+++ src/StaticRects/Map.java
@@ -13,6 +13,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import javax.swing.JComponent;
 
 /**
@@ -26,29 +27,62 @@ public class Map extends JComponent {
     public int mapLength = 10;
     public int mapWidth = 10;
     
+    BufferedImage input, output, testImage;
+    static BufferedImage testOut; 
+    BufferedImage[] testArray;
+    BufferedImage[][] tileArray = new BufferedImage[16][16];
+    int imageWidth = 0, imageHeight = 0;
+    int subImageWidth = 8;
+    int subImageHeight = 12;
+    
+    TileSplitter splitter;
+
+    public Map() {
+        this.splitter = new TileSplitter();
+        
+        splitter.LoadImage();
+        // testOut = splitter.SaveImage(); // Capture and save the SubImage
+        testOut = splitter.getRandomImage();
+        splitter.ShowImage(testOut);
+    }    
 
 //   public void paint(Graphics g) {
 //      g.fillRect (15, 15, 50, 50);
 //   }
     
-
+    /*        g.drawImage(bi,
+                    dx, dy, 
+                    x+cellWidth, dy+cellHeight,
+                    sx, sy,
+                    sx+cellWidth, sy+cellHeight,
+                    null);
+    */
     
+    @Override
     public void paint(Graphics g) {
         
         Graphics2D g2d = (Graphics2D)g;
         float thickness = 3;
+        int currentX = 3;
+        int currentY = 3;
   //      g2d.setStroke(new BasicStroke(thickness));
         
-            for(int i = 0; i < 300; i++) {   
-                for (int j = 0; j < 300; j++) {
+            for(int i = 0; i < 30; i++) {   
+                for (int j = 0; j < 30; j++) {
                     g2d.setColor(Color.black);
                     g2d.drawRect(i * tileWidth, j * tileHeight, tileWidth, tileHeight);
                     g2d.setStroke(new BasicStroke(thickness));
                     g2d.setColor(Color.yellow);
+                    System.out.println("currentX = " + currentX + ", CurrentY " + currentY);
+                    g2d.drawImage(testOut, 7, 12, null);
                     g2d.fillRect(i * tileWidth, j * tileHeight, tileWidth, tileHeight);  
+                    currentX += 3;
+                    currentY += 3;
                 }
             } 
-    } 
+    }        
+        
+
 }
 
 /*Graphics2D g2;
---
+++ src/StaticRects/TileSplitter.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StaticRects;
+
+import java.awt.image.*; 
+import javax.imageio.*; 
+import java.io.*;
+import java.util.Random;
+
+public class TileSplitter {
+
+    
+    BufferedImage input, output, testImage;
+    static BufferedImage testOut; 
+    BufferedImage[] testArray;
+    BufferedImage[][] tileArray = new BufferedImage[16][16];
+    int imageWidth = 0, imageHeight = 0;
+    int subImageWidth = 8;
+    int subImageHeight = 12;
+
+    
+    //
+    public void LoadImage() {
+        try {
+            input = ImageIO.read(new File("c:\\java\\images\\curses.bmp"));
+            imageWidth = input.getWidth();
+            imageHeight = input.getHeight();
+            System.out.println("imageWidth = " + imageWidth);
+            System.out.println("imageHeight = " + imageHeight);
+            //       output = ImageIO.write(input, "c:\\java\\images\\out.bmp", null)
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    // method to capture and save a SubImage of "input"
+    public void ShowImage(BufferedImage test) 
+    {
+        try {
+            BufferedImage output = test;
+            File fo = new File("C:\\java\\images\\newimages20170104.png"); 
+            ImageIO.write(output, "JPG", fo);
+        }
+        catch (IOException e)
+        {
+            System.out.println("Writing to output failed.");
+        }
+    }
+    
+    
+    // Initial method to capture and save a Sub-Image of "input"
+    public BufferedImage SaveImage() 
+    {
+        int x = 0, y = 0, w = subImageWidth, h = subImageHeight;     // width is 8, height is 12
+
+        try {
+            for (int row = 0; row <= 15; row++) {
+                System.out.println("row = " + row);
+                for (int column = 0; column <= 15; column++) // was column <= 15
+                {
+                    System.out.println("column = " + column + ", x = " + x + ", y = " + y);
+                    tileArray[row][column] = input.getSubimage(x, y, w, h);
+                    x = x + 8;                                            
+
+                }
+                x = 0;
+                y = y + 12;
+            }
+            return testImage = tileArray[7][7];
+        } finally {
+        }
+    }
+
+    // Returns a random 8 x 12 sub-image from our Buffered Image input
+    public BufferedImage getRandomImage() {
+
+        Random randomRow = new Random();
+        int rowIndex = (randomRow.nextInt(16)) * 8;
+
+        Random randomColumn = new Random();
+        int columnIndex = (randomColumn.nextInt(16)) * 12;
+
+        System.out.println("rowIndex = " + rowIndex + ", columnIndex = " + columnIndex);
+
+        return input.getSubimage(rowIndex, columnIndex, subImageWidth, subImageHeight);
+    }
+
+    
+    public static void main(String args[]) {
+        TileSplitter splitter = new TileSplitter();
+        splitter.LoadImage();
+        //	testOut = splitter.SaveImage(); // Capture and save the SubImage
+        testOut = splitter.getRandomImage();
+        splitter.ShowImage(testOut);
+    }
+}
